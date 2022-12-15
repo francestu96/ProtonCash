bea80179f301351d281c2a428192be95180e90d7
francestu96 <francestu96@gmail.com>
2022-12-15 17:38:36 +0100

    fix missing import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import { Default } from 'components/templates/Default';
 import { Home } from 'components/templates/home';
+import { NextPage } from 'next';
 
-const HomePage: NextPage<any> = (props) => {
+const HomePage: NextPage = () => {
   return (
     <Default pageName="Home">
-      <Home {...props}/>
+      <Home/>
     </Default>
   );
 };
